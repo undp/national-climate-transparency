57a58d96d9fb0f850c1d1c9017c07600df748927
YathurshanT <yathurshant@xeptagon.com>
2023-06-20 09:16:35 +0530

    CHanges in investment body

--- web/src/Components/InvestmentBody/investmentBody.tsx
+++ web/src/Components/InvestmentBody/investmentBody.tsx
@@ -20,6 +20,8 @@ import { InvestmentStatus } from '../../Casl/enums/investment.status';
 import { useConnection } from '../../Context/ConnectionContext/connectionContext';
 import { Skeleton, message } from 'antd';
 import { DocumentStatus } from '../../Casl/enums/document.status';
+import RejectDocumentationConfirmationModel from '../Models/rejectDocumentForm';
+import { useTranslation } from 'react-i18next';
 
 export interface InvestmentBodyProps {
   data: any;
@@ -28,8 +30,12 @@ export interface InvestmentBodyProps {
 const InvestmentBody: FC<InvestmentBodyProps> = (props: InvestmentBodyProps) => {
   const { data } = props;
   const { get, put, post } = useConnection();
+  const { t } = useTranslation(['programme']);
   const [loading, setLoading] = useState<boolean>(false);
   const [investmentData, setInvestmentData] = useState<any>({});
+  const [openRejectDocConfirmationModal, setOpenRejectDocConfirmationModal] = useState(false);
+  const [actionInfo, setActionInfo] = useState<any>({});
+  const [rejectDocData, setRejectDocData] = useState<any>({});
 
   useEffect(() => {
     setInvestmentData(data);
@@ -39,7 +45,7 @@ const InvestmentBody: FC<InvestmentBodyProps> = (props: InvestmentBodyProps) => 
   const investmentAction = async (type: any, id: any) => {
     setLoading(true);
     try {
-      if (type === 'approve') {
+      if (type === InvestmentStatus.APPROVED) {
         const response: any = await post('national/programme/investmentApprove', {
           requestId: id,
         });
@@ -49,9 +55,21 @@ const InvestmentBody: FC<InvestmentBodyProps> = (props: InvestmentBodyProps) => 
           duration: 4,
           style: { textAlign: 'right', marginRight: 15, marginTop: 10 },
         });
+        investmentData.status = InvestmentStatus.APPROVED;
+        setInvestmentData({ ...investmentData });
+      } else if (type === InvestmentStatus.REJECTED) {
+        const response: any = await post('national/programme/investmentReject', {
+          requestId: id,
+        });
+        message.open({
+          type: 'success',
+          content: response?.message,
+          duration: 4,
+          style: { textAlign: 'right', marginRight: 15, marginTop: 10 },
+        });
+        investmentData.status = InvestmentStatus.REJECTED;
+        setInvestmentData({ ...investmentData });
       }
-      investmentData.status = DocumentStatus.ACCEPTED;
-      setInvestmentData({ ...investmentData });
     } catch (error: any) {
       message.open({
         type: 'error',
@@ -61,63 +79,98 @@ const InvestmentBody: FC<InvestmentBodyProps> = (props: InvestmentBodyProps) => 
       });
     } finally {
       setLoading(false);
+      setOpenRejectDocConfirmationModal(false);
     }
+  };
+
+  const handleOk = () => {
+    investmentAction(InvestmentStatus.REJECTED, rejectDocData?.id);
+  };
+
+  const handleCancel = () => {
+    setOpenRejectDocConfirmationModal(false);
   };
 
   return loading ? (
     <Skeleton />
   ) : (
-    <div className="investment-body">
-      <div className="invester">
-        <div className="name-and-progress">
-          <div className="name">{investmentData?.invester}</div>
-          <div className="progress">
-            {investmentData?.status === InvestmentStatus.APPROVED ? (
-              <CheckCircleOutlined className="common-progress-icon" style={{ color: '#5DC380' }} />
-            ) : investmentData?.status === InvestmentStatus.PENDING ? (
-              <>
-                <LikeOutlined
-                  onClick={() => investmentAction('approve', investmentData?.requestId)}
+    <>
+      <div className="investment-body">
+        <div className="invester">
+          <div className="name-and-progress">
+            <div className="name">{investmentData?.invester}</div>
+            <div className="progress">
+              {investmentData?.status === InvestmentStatus.APPROVED ? (
+                <CheckCircleOutlined
                   className="common-progress-icon"
-                  style={{ color: '#976ED7' }}
+                  style={{ color: '#5DC380' }}
                 />
-                <DislikeOutlined
-                  className="common-progress-icon margin-left-1"
-                  style={{ color: '#FD6F70' }}
-                />
-              </>
-            ) : null}
+              ) : investmentData?.status === InvestmentStatus.PENDING ? (
+                <>
+                  <LikeOutlined
+                    onClick={() =>
+                      investmentAction(InvestmentStatus.APPROVED, investmentData?.requestId)
+                    }
+                    className="common-progress-icon"
+                    style={{ color: '#976ED7' }}
+                  />
+                  <DislikeOutlined
+                    onClick={() => {
+                      setRejectDocData({ id: investmentData?.requestId });
+                      setActionInfo({
+                        action: 'Reject',
+                        headerText: `${t('programme:rejectInvestmentHeader')}`,
+                        text: `${t('programme:rejectDocBody')}`,
+                        type: 'reject',
+                        icon: <DislikeOutlined />,
+                      });
+                      setOpenRejectDocConfirmationModal(true);
+                    }}
+                    className="common-progress-icon margin-left-1"
+                    style={{ color: '#FD6F70' }}
+                  />
+                </>
+              ) : null}
+            </div>
+          </div>
+          <div className="time">
+            {moment(parseInt(investmentData?.createdAt)).format('DD MMMM YYYY @ HH:mm')}
           </div>
         </div>
-        <div className="time">
-          {moment(parseInt(investmentData?.createdAt)).format('DD MMMM YYYY @ HH:mm')}
+        <div className="amount">${addCommSep(investmentData?.amount)}</div>
+        <div className="actions">
+          <div className="actions-icon-container">
+            {investmentData?.type === InvestmentType.PUBLIC ? (
+              <EyeOutlined className="action-icons" />
+            ) : (
+              <EyeInvisibleOutlined className="action-icons" />
+            )}
+          </div>
+          <div className="actions-icon-container">
+            {investmentData?.level === InvestmentLevel.INTERNATIONAL ? (
+              <GlobalOutlined className="action-icons" />
+            ) : (
+              <FlagOutlined className="action-icons" />
+            )}
+          </div>
+          <div className="actions-icon-container">
+            {investmentData?.stream === InvestmentStream.CLIMATE_FINANCE ? (
+              <BankOutlined className="action-icons" />
+            ) : (
+              <LineChartOutlined className="action-icons" />
+            )}
+          </div>
         </div>
       </div>
-      <div className="amount">${addCommSep(investmentData?.amount)}</div>
-      <div className="actions">
-        <div className="actions-icon-container">
-          {investmentData?.type === InvestmentType.PUBLIC ? (
-            <EyeOutlined className="action-icons" />
-          ) : (
-            <EyeInvisibleOutlined className="action-icons" />
-          )}
-        </div>
-        <div className="actions-icon-container">
-          {investmentData?.level === InvestmentLevel.INTERNATIONAL ? (
-            <GlobalOutlined className="action-icons" />
-          ) : (
-            <FlagOutlined className="action-icons" />
-          )}
-        </div>
-        <div className="actions-icon-container">
-          {investmentData?.stream === InvestmentStream.CLIMATE_FINANCE ? (
-            <BankOutlined className="action-icons" />
-          ) : (
-            <LineChartOutlined className="action-icons" />
-          )}
-        </div>
-      </div>
-    </div>
+      <RejectDocumentationConfirmationModel
+        actionInfo={actionInfo}
+        onActionConfirmed={handleOk}
+        onActionCanceled={handleCancel}
+        openModal={openRejectDocConfirmationModal}
+        errorMsg={''}
+        loading={loading}
+      />
+    </>
   );
 };
 
