58da912068faceb53ac3108a206b1c52b46f8493
YathurshanT <yathurshant@xeptagon.com>
2023-08-11 10:02:13 +0530

    carbon lib update and permission changes

--- web/src/Casl/ability.ts
+++ web/src/Casl/ability.ts
@@ -43,7 +43,10 @@ export const updateUserAbility = (ability: AppAbility, user: User) => {
       can([Action.Delete], Company);
       can([Action.Create], Company);
       can(Action.Update, Company, { companyId: { $eq: user.companyId } });
-    } else if (user.role === Role.Admin && user.companyRole === CompanyRole.GOVERNMENT) {
+    } else if (
+      user.role === Role.Admin &&
+      (user.companyRole === CompanyRole.GOVERNMENT || user.companyRole === CompanyRole.MINISTRY)
+    ) {
       can(Action.Manage, User, { role: { $ne: Role.Root } });
       cannot(Action.Update, User, ['role', 'apiKey', 'password', 'companyRole', 'email'], {
         id: { $eq: user.id },
@@ -54,10 +57,12 @@ export const updateUserAbility = (ability: AppAbility, user: User) => {
       cannot(Action.Update, Company, ['companyRole']);
       can(Action.Delete, Company);
       can(Action.Create, Company);
-    } else if (user.role === Role.Admin && user.companyRole !== CompanyRole.GOVERNMENT) {
       if (user.companyRole === CompanyRole.MINISTRY) {
-        can(Action.Create, Company);
+        cannot([Action.Update, Action.Delete, Action.Read], User, {
+          companyId: { $ne: user.companyId },
+        });
       }
+    } else if (user.role === Role.Admin && user.companyRole !== CompanyRole.GOVERNMENT) {
       can(Action.Manage, User, { role: { $ne: Role.Root } });
       cannot([Action.Update, Action.Delete, Action.Read], User, {
         companyId: { $ne: user.companyId },
@@ -72,7 +77,10 @@ export const updateUserAbility = (ability: AppAbility, user: User) => {
       cannot(Action.Update, Company, ['companyRole']);
       cannot(Action.Create, Company);
     } else {
-      if (user.companyRole === CompanyRole.GOVERNMENT) {
+      if (
+        user.companyRole === CompanyRole.GOVERNMENT ||
+        user.companyRole === CompanyRole.MINISTRY
+      ) {
         can(Action.Read, User);
       } else {
         can(Action.Read, User, { companyId: { $eq: user.companyId } });
