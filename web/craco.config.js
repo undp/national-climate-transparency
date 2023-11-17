2979ea45698eb0c20481e9c28a5f9b0c0e4dbde4
leshanw <leshanw@xeptagon.com>
2023-11-17 13:59:39 +0530

    CARBON-339 : Fixed ssue in craco.config.js

--- web/craco.config.js
+++ web/craco.config.js
@@ -8,20 +8,6 @@ module.exports = {
       // this option lets us display the map-pin marker layer - without this it does not work: https://github.com/visgl/react-map-gl/issues/1266
       ignore: ['./node_modules/mapbox-gl/dist/mapbox-gl.js'],
     },
-    plugin: {
-      overrideWebpackConfig: ({ webpackConfig }) => {
-        // Copying locale files from carbon-library node module to public folder 
-        const localeSrcDir = path.resolve(__dirname, 'node_modules/@undp/carbon-library/dist/locales');
-        const localeDestDir = path.resolve(__dirname, 'public/locales');
-
-        // Ensure the destination directory exists, then copy the files
-        if (fs.existsSync(localeSrcDir)) {
-          fs.ensureDirSync(localeDestDir);
-          fs.copySync(localeSrcDir, localeDestDir);
-        }
-        return webpackConfig;
-      }
-    }
   },
   plugins: [
     {
@@ -41,5 +27,22 @@ module.exports = {
         },
       },
     },
+    {
+      plugin: {
+        overrideWebpackConfig: ({ webpackConfig }) => {
+
+          // Copying locale files from carbon-library dist in the node_modules folder to public folder  
+          const localeSrcDir = path.resolve(__dirname, 'node_modules/@undp/carbon-library/dist/locales');
+          const localeDestDir = path.resolve(__dirname, 'public/locales');
+
+          // Ensure the destination directory exists, then copy the files
+          if (fs.existsSync(localeSrcDir)) {
+            fs.ensureDirSync(localeDestDir);
+            fs.copySync(localeSrcDir, localeDestDir);
+          }
+          return webpackConfig;
+        }
+      }
+    }
   ],
 };
