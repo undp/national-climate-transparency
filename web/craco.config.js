85b7bc74dddb5b7d2aac308219e8ec145b305c90
leshanw <leshanw@xeptagon.com>
2023-11-17 12:47:43 +0530

    CARBON-339 : Moved translation files to carbon library

--- web/craco.config.js
+++ web/craco.config.js
@@ -1,4 +1,6 @@
 const CracoLessPlugin = require('craco-less');
+const path = require('path');
+const fs = require('fs-extra');
 
 module.exports = {
   babel: {
@@ -6,6 +8,20 @@ module.exports = {
       // this option lets us display the map-pin marker layer - without this it does not work: https://github.com/visgl/react-map-gl/issues/1266
       ignore: ['./node_modules/mapbox-gl/dist/mapbox-gl.js'],
     },
+    plugin: {
+      overrideWebpackConfig: ({ webpackConfig }) => {
+        // Copying locale files from carbon-library node module to public folder 
+        const localeSrcDir = path.resolve(__dirname, 'node_modules/@undp/carbon-library/dist/locales');
+        const localeDestDir = path.resolve(__dirname, 'public/locales');
+
+        // Ensure the destination directory exists, then copy the files
+        if (fs.existsSync(localeSrcDir)) {
+          fs.ensureDirSync(localeDestDir);
+          fs.copySync(localeSrcDir, localeDestDir);
+        }
+        return webpackConfig;
+      }
+    }
   },
   plugins: [
     {
