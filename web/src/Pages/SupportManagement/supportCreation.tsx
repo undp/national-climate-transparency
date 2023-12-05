12b36a698f642738e9c33075e452cf8fdaeb124d
leshanw <leshanw@xeptagon.com>
2023-12-05 13:42:07 +0530

    CARBON-382 : Moved context classes to the library

--- web/src/Pages/SupportManagement/supportCreation.tsx
+++ web/src/Pages/SupportManagement/supportCreation.tsx
@@ -1,9 +1,6 @@
 import { SupportCreationComponent } from '@undp/carbon-library';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { useUserContext } from '../../Context/UserInformationContext/userInformationContext';
 import { useTranslation } from 'react-i18next';
-import { useConnection } from '../../Context/ConnectionContext/connectionContext';
-import { useSettingsContext } from '../../Context/SettingsContext/settingsContext';
 
 const AddSupportComponent = () => {
   const navigate = useNavigate();
@@ -20,10 +17,7 @@ const AddSupportComponent = () => {
   return (
     <SupportCreationComponent
       t={t}
-      useConnection={useConnection}
-      userInfoState={useUserContext}
       useLocation={useLocation}
-      useSettingsContext={useSettingsContext}
       onNavigateToProgrammeManagementView={onNavigateToProgrammeManagementView}
       onNavigateToProgrammeView={onNavigateToProgrammeView}
     ></SupportCreationComponent>
