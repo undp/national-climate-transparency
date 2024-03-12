c00965f9103d704913e92d1c75a5f733f802b0d0
MeelanB <155341696+MeelanB@users.noreply.github.com>
2024-03-12 11:28:40 +0530

    migrations package json updated, docker-compose updated

--- backend/services/src/typeorm.config.service.ts
+++ backend/services/src/typeorm.config.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+import configuration from "./configuration";
+import { DataSource, DataSourceOptions } from "typeorm";
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
-
   constructor(private configService: ConfigService) {}
-  
+
   createTypeOrmOptions(): TypeOrmModuleOptions {
-    return this.configService.get<any>('database');
+    return this.configService.get<any>("database");
   }
 }
+
+const dbConfig = {
+  ...configuration().database,
+  migrations: [`src/migrations/*.{ts,js}`],
+  migrationsRun: true,
+};
+
+export default new DataSource(dbConfig as DataSourceOptions);
