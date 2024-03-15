857c10877533d70b400c55bbc92d481727867b67
MeelanB <155341696+MeelanB@users.noreply.github.com>
2024-03-15 16:53:09 +0530

    KPI grid delete button alignments

--- web/src/Components/KPI/KpiGrid.tsx
+++ web/src/Components/KPI/KpiGrid.tsx
@@ -1,5 +1,5 @@
 import { DeleteOutlined } from '@ant-design/icons';
-import { Row, Col, Input, Button } from 'antd';
+import { Row, Col, Input, Card } from 'antd';
 
 interface Props {
   gutterSize: number;
@@ -24,29 +24,32 @@ const KpiGrid: React.FC<Props> = ({ gutterSize, rowHeight, rowBottomMargin, fiel
         </Row>
       </Col>
       <Col span={12} style={{ height: rowHeight }}>
-        <Row gutter={gutterSize} style={{ marginBottom: rowBottomMargin }}>
-          <Col span={10} style={{ height: rowHeight }}>
+        <Row gutter={15} style={{ marginBottom: rowBottomMargin }}>
+          <Col span={11} style={{ height: rowHeight }}>
             <div style={{ color: '#3A3541', opacity: 0.8, margin: '8px 0' }}>{'Achieved'}</div>
             <Input style={{ height: fieldHeight }} />
           </Col>
-          <Col span={10} style={{ height: rowHeight }}>
+          <Col span={11} style={{ height: rowHeight }}>
             <div style={{ color: '#3A3541', opacity: 0.8, margin: '8px 0' }}>{'Expected'}</div>
             <Input style={{ height: fieldHeight }} />
           </Col>
-          <Col span={4} style={{ height: rowHeight }}>
-            <Button
-              block
+          <Col span={2} style={{ height: rowHeight }}>
+            <Card
               style={{
+                display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
                 padding: '0px',
-                width: '10px',
-                height: fieldHeight,
-                marginTop: '38px',
+                width: '31px',
+                height: '31px',
+                marginTop: '43px',
+                borderWidth: '1px',
+                borderRadius: '4px',
+                borderColor: '#d9d9d9',
               }}
             >
               <DeleteOutlined style={{ cursor: 'pointer', color: '#3A3541', opacity: 0.8 }} />
-            </Button>
+            </Card>
           </Col>
         </Row>
       </Col>
