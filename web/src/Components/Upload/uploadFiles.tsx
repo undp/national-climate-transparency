3611b38fea02d40dc993eabcbfc855ac6695d846
MeelanB <155341696+MeelanB@users.noreply.github.com>
2024-04-03 13:36:48 +0530

    Remove confirmation popup added

--- web/src/Components/Upload/uploadFiles.tsx
+++ web/src/Components/Upload/uploadFiles.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import { RcFile, UploadFile } from 'antd/lib/upload/interface';
-import { Button, Col, Row, Upload } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
-import DeleteCard from '../Card/deleteCard';
+import { Button, Card, Col, Row, Upload } from 'antd';
+import { DeleteOutlined, UploadOutlined } from '@ant-design/icons';
+import ConfirmPopup from '../Popups/Confirmation/confirmPopup';
+import './uploadFiles.scss';
+import { XOctagon } from 'react-bootstrap-icons';
 
 interface Props {
   uploadedFiles: { id: string; title: string; data: string }[];
@@ -30,6 +32,8 @@ const UploadFileGrid: React.FC<Props> = ({
   isView,
 }) => {
   const [documentList, setDocumentList] = useState<UploadFile[]>([]);
+  const [confirmOpen, setConfirmOpen] = useState<boolean>(false);
+  const [whichFile, setWhichFile] = useState<number>();
 
   const handleFileRead = (file: RcFile): Promise<string> =>
     new Promise((resolve, reject) => {
@@ -41,6 +45,11 @@ const UploadFileGrid: React.FC<Props> = ({
 
   const onChange = ({ fileList: newFileList }: { fileList: UploadFile[] }) => {
     setDocumentList(newFileList);
+  };
+
+  const handleDeleteClick = (fileId: any) => {
+    setConfirmOpen(true);
+    setWhichFile(fileId);
   };
 
   const handleDelete = (fileId: any) => {
@@ -63,32 +72,48 @@ const UploadFileGrid: React.FC<Props> = ({
   };
 
   return (
-    <Row gutter={[horizontalGutter, verticalGutter]} style={style}>
-      <Col span={3} style={{ height: height }}>
-        <Upload {...props}>
-          <Button
-            icon={<UploadOutlined />}
-            style={{ width: '120px', height: height, color: '#3A3541', opacity: 0.8 }}
-            disabled={isView}
-          >
-            {buttonText}
-          </Button>
-        </Upload>
-      </Col>
-      <Col span={21}>
-        <Row gutter={[horizontalGutter, verticalGutter]}>
-          {documentList.map((file: any) => (
-            <Col key={file.uid} span={8} style={{ height: height }}>
-              <DeleteCard
-                fileName={file.name.slice(0, 20)}
-                fileId={file.uid}
-                handleDelete={handleDelete}
-              ></DeleteCard>
-            </Col>
-          ))}
-        </Row>
-      </Col>
-    </Row>
+    <div className="upload-files">
+      <ConfirmPopup
+        icon={<XOctagon style={{ color: '#ff4d4f', fontSize: '120px' }} />}
+        isDanger={true}
+        content={{
+          primaryMsg: 'Are you sure you want to remove this document?',
+          secondaryMsg: 'You can’t undo this action',
+          cancelTitle: 'cancel',
+          actionTitle: 'remove',
+        }}
+        actionRef={whichFile}
+        doAction={handleDelete}
+        open={confirmOpen}
+        setOpen={setConfirmOpen}
+      />
+      <Row gutter={[horizontalGutter, verticalGutter]} style={style}>
+        <Col span={3} style={{ height: height }}>
+          <Upload {...props}>
+            <Button className="upload-button" icon={<UploadOutlined />} disabled={isView}>
+              {buttonText}
+            </Button>
+          </Upload>
+        </Col>
+        <Col span={21}>
+          <Row gutter={[horizontalGutter, verticalGutter]}>
+            {documentList.map((file: any) => (
+              <Col key={file.uid} span={8} style={{ height: height }}>
+                <Card className="file-card">
+                  <div className="file-content">
+                    <span>{file.name.slice(0, 20)}</span>
+                    <DeleteOutlined
+                      className="delete-icon"
+                      onClick={() => handleDeleteClick(file.uid)}
+                    />
+                  </div>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
