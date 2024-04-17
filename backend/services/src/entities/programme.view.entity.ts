ac6d0c6b394a48e98c67a7db845c9cf5ebb22721
MeelanB <155341696+MeelanB@users.noreply.github.com>
2024-04-17 15:19:35 +0530

    docker-compose dependency added for migrations
    Indentations fixed in program and project views

--- backend/services/src/entities/programme.view.entity.ts
+++ backend/services/src/entities/programme.view.entity.ts
@@ -2,39 +2,38 @@ import { Index, ViewColumn, ViewEntity } from "typeorm"
 
 export const programmeViewSQL = `
 SELECT 
-			p."programmeId" AS id,
-			ARRAY_AGG(DISTINCT prj.type) FILTER (WHERE prj.type IS NOT NULL) AS types,
-			ARRAY_AGG(DISTINCT prj."internationalImplementingEntities") FILTER (WHERE prj."internationalImplementingEntities" IS NOT NULL) AS "internationalImplementingEntities",
-			ARRAY_AGG(DISTINCT prj."recipientEntities") FILTER (WHERE prj."recipientEntities" IS NOT NULL) AS "recipientEntities",
-			reductionSum."achievedGHGReduction" AS "achievedGHGReduction",
-			reductionSum."expectedGHGReduction" AS "expectedGHGReduction"
+		p."programmeId" AS id,
+		ARRAY_AGG(DISTINCT prj.type) FILTER (WHERE prj.type IS NOT NULL) AS types,
+		ARRAY_AGG(DISTINCT prj."internationalImplementingEntities") FILTER (WHERE prj."internationalImplementingEntities" IS NOT NULL) AS "internationalImplementingEntities",
+		ARRAY_AGG(DISTINCT prj."recipientEntities") FILTER (WHERE prj."recipientEntities" IS NOT NULL) AS "recipientEntities",
+		reductionSum."achievedGHGReduction" AS "achievedGHGReduction",
+		reductionSum."expectedGHGReduction" AS "expectedGHGReduction"
+FROM 
+		programme p
+LEFT JOIN (
+	SELECT 
+		prje."projectId" AS id,
+		prje.type AS type,
+		UNNEST(prje."internationalImplementingEntities") AS "internationalImplementingEntities",
+		UNNEST(prje."recipientEntities") AS "recipientEntities",
+		prje."programmeId"
 	FROM 
-			programme p
-	LEFT JOIN (
-		SELECT 
-			prje."projectId" AS id,
-			prje.type AS type,
-			UNNEST(prje."internationalImplementingEntities") AS "internationalImplementingEntities",
-			UNNEST(prje."recipientEntities") AS "recipientEntities",
-			prje."programmeId"
-	FROM 
-			project prje
-		GROUP BY 
-			prje."projectId"
-	) prj ON p."programmeId" = prj."programmeId"
-	
-	LEFT JOIN (
-    SELECT 
-        "programmeId",
-        SUM("achievedGHGReduction") AS "achievedGHGReduction",
-				SUM("expectedGHGReduction") AS "expectedGHGReduction"
-    FROM 
-        project
-    GROUP BY 
-        "programmeId"
-	) reductionSum ON  p."programmeId" = reductionSum."programmeId"
+		project prje
 	GROUP BY 
-			p."programmeId", reductionsum."achievedGHGReduction", reductionsum."expectedGHGReduction";`
+		prje."projectId"
+) prj ON p."programmeId" = prj."programmeId"
+LEFT JOIN (
+	SELECT 
+		"programmeId",
+		SUM("achievedGHGReduction") AS "achievedGHGReduction",
+				SUM("expectedGHGReduction") AS "expectedGHGReduction"
+	FROM 
+		project
+	GROUP BY 
+		"programmeId"
+) reductionSum ON  p."programmeId" = reductionSum."programmeId"
+GROUP BY 
+	p."programmeId", reductionsum."achievedGHGReduction", reductionsum."expectedGHGReduction";`
 
 @ViewEntity({
 	name: 'programme_view_entity',
--- backend/services/src/entities/project.view.entity.ts
+++ backend/services/src/entities/project.view.entity.ts
@@ -1,43 +1,43 @@
 import { Index, ViewColumn, ViewEntity } from "typeorm"
 
 export const projectViewSQL = `
+SELECT 
+    prj."projectId" AS id,
+    ARRAY_AGG(DISTINCT fullact."techTypes") AS "technologyTypes",
+    ARRAY_AGG(DISTINCT fullact."meansOfImplementation") AS "meansOfImplementation",
+    SUM(fullact."requiredAmount") AS "estimatedAmount",
+    SUM(fullact."receivedAmount") AS "receivedAmount",
+    SUM(fullact."requiredAmountDomestic") AS "estimatedAmountDomestic",
+    SUM(fullact."receivedAmountDomestic") AS "receivedAmountDomestic"
+FROM 
+    project prj
+LEFT JOIN (
     SELECT 
-        prj."projectId" AS id,
-        ARRAY_AGG(DISTINCT fullact."techTypes") AS "technologyTypes",
-        ARRAY_AGG(DISTINCT fullact."meansOfImplementation") AS "meansOfImplementation",
-        SUM(fullact."requiredAmount") AS "estimatedAmount",
-        SUM(fullact."receivedAmount") AS "receivedAmount",
-		SUM(fullact."requiredAmountDomestic") AS "estimatedAmountDomestic",
-		SUM(fullact."receivedAmountDomestic") AS "receivedAmountDomestic"
+        act."activityId",
+        act."parentId" AS "projectId",
+        act."technologyType" AS "techTypes",
+        act."meansOfImplementation" AS "meansOfImplementation",
+        sup."requiredAmount",
+        sup."receivedAmount",
+        sup."requiredAmountDomestic",
+        sup."receivedAmountDomestic"
     FROM 
-        project prj
+        activity act
     LEFT JOIN (
         SELECT 
-            act."activityId",
-            act."parentId" AS "projectId",
-            act."technologyType" AS "techTypes",
-            act."meansOfImplementation" AS "meansOfImplementation",
-            sup."requiredAmount",
-            sup."receivedAmount",
-			sup."requiredAmountDomestic",
-            sup."receivedAmountDomestic"
+            "activityId",
+            SUM("requiredAmount") AS "requiredAmount",
+            SUM("receivedAmount") AS "receivedAmount",
+            SUM("requiredAmountDomestic") AS "requiredAmountDomestic",
+            SUM("receivedAmountDomestic") AS "receivedAmountDomestic"
         FROM 
-            activity act
-        LEFT JOIN (
-            SELECT 
-                "activityId",
-                SUM("requiredAmount") AS "requiredAmount",
-                SUM("receivedAmount") AS "receivedAmount",
-				SUM("requiredAmountDomestic") AS "requiredAmountDomestic",
-                SUM("receivedAmountDomestic") AS "receivedAmountDomestic"
-            FROM 
-                support
-            GROUP BY 
-                "activityId"
-            ) sup ON act."activityId" = sup."activityId"
-        ) fullact ON prj."projectId" = fullact."projectId"
-    GROUP BY 
-        prj."projectId";`
+            support
+        GROUP BY 
+            "activityId"
+        ) sup ON act."activityId" = sup."activityId"
+    ) fullact ON prj."projectId" = fullact."projectId"
+GROUP BY 
+    prj."projectId";`
 
 @ViewEntity({
     name: 'project_view_entity',
