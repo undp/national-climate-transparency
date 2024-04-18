6a870be184b4218bb44858de8c255d094f1dd7e7
MeelanB <155341696+MeelanB@users.noreply.github.com>
2024-04-18 15:53:06 +0530

    Program View Entity fixed to show GHG Emission values

--- backend/services/src/entities/programme.view.entity.ts
+++ backend/services/src/entities/programme.view.entity.ts
@@ -3,46 +3,44 @@ import { Index, ViewColumn, ViewEntity } from "typeorm"
 export const programmeViewSQL = `
 SELECT 
 		p."programmeId" AS id,
-		ARRAY_AGG(DISTINCT prj.type) FILTER (WHERE prj.type IS NOT NULL) AS types,
-		ARRAY_AGG(DISTINCT prj."internationalImplementingEntities") FILTER (WHERE prj."internationalImplementingEntities" IS NOT NULL) AS "internationalImplementingEntities",
-		ARRAY_AGG(DISTINCT prj."recipientEntities") FILTER (WHERE prj."recipientEntities" IS NOT NULL) AS "recipientEntities",
-		reductionSum."achievedGHGReduction" AS "achievedGHGReduction",
-		reductionSum."expectedGHGReduction" AS "expectedGHGReduction"
+		ARRAY_AGG(DISTINCT fullprj.type) FILTER (WHERE fullprj.type IS NOT NULL) AS types,
+		ARRAY_AGG(DISTINCT fullprj."internationalImplementingEntities") FILTER (WHERE fullprj."internationalImplementingEntities" IS NOT NULL) AS "internationalImplementingEntities",
+		ARRAY_AGG(DISTINCT fullprj."recipientEntities") FILTER (WHERE fullprj."recipientEntities" IS NOT NULL) AS "recipientEntities",
+		SUM(fullprj."achievedGHGReduction") AS "achievedGHGReduction",
+		SUM(fullprj."expectedGHGReduction") AS "expectedGHGReduction"
 FROM 
 		programme p
 LEFT JOIN (
 	SELECT 
-		prje."projectId" AS id,
-		prje.type AS type,
-		UNNEST(prje."internationalImplementingEntities") AS "internationalImplementingEntities",
-		UNNEST(prje."recipientEntities") AS "recipientEntities",
-		prje."programmeId"
+		prj."projectId" AS id,
+		prj.type AS type,
+		prj."programmeId",
+		UNNEST(prj."internationalImplementingEntities") AS "internationalImplementingEntities",
+		UNNEST(prj."recipientEntities") AS "recipientEntities",
+		MAX(p_v_e."achievedGHGReduction") AS "achievedGHGReduction",
+		MAX(p_v_e."expectedGHGReduction") AS "expectedGHGReduction"
 	FROM 
-		project prje
+		project prj
+	LEFT JOIN (
+		SELECT 
+			id,
+			"achievedGHGReduction",
+			"expectedGHGReduction"
+		FROM 
+			project_view_entity
+		) p_v_e ON prj."projectId" = p_v_e.id
 	GROUP BY 
-		prje."projectId"
-) prj ON p."programmeId" = prj."programmeId"
-LEFT JOIN (
-	SELECT 
-		"programmeId",
-		SUM("achievedGHGReduction") AS "achievedGHGReduction",
-				SUM("expectedGHGReduction") AS "expectedGHGReduction"
-	FROM 
-		project
-	GROUP BY 
-		"programmeId"
-) reductionSum ON  p."programmeId" = reductionSum."programmeId"
+		prj."projectId"
+	) fullprj ON p."programmeId" = fullprj."programmeId"
 GROUP BY 
-	p."programmeId", reductionsum."achievedGHGReduction", reductionsum."expectedGHGReduction";`
+	p."programmeId";`
 
 @ViewEntity({
 	name: 'programme_view_entity',
 	materialized: true,
 	expression: programmeViewSQL,
 	synchronize: false,
-
 })
-
 export class ProgrammeViewEntity {
 	@Index()
 	@ViewColumn()
