86ee97bfa3e6d793c34d7b5a8ae47db8a91a51da
PruthuviXeptagon <161282025+PruthuviXeptagon@users.noreply.github.com>
2024-05-15 10:47:44 +0530

    Added Frequently Asked Question Card

--- web/src/Pages/Faq/faq.tsx
+++ web/src/Pages/Faq/faq.tsx
@@ -1,5 +1,38 @@
+import { useTranslation } from 'react-i18next';
+import './faq.scss';
+import { Collapse } from 'antd';
+
+const { Panel } = Collapse;
+
 const faq = () => {
-  return <div>FAQ Component</div>;
+  const { t } = useTranslation(['faq']);
+
+  return (
+    <div className="content-container">
+      <div className="title-bar">
+        <div className="body-title">{t('faqMenu')}</div>
+      </div>
+      <div className="content-card">
+        <div className="section-header">{t('freqAskedQ')}</div>
+        <div className="questions">
+          <Collapse bordered={false}>
+            <Panel header={t('question1')} key="1">
+              <div className="answers">{t('answer1')}</div>
+            </Panel>
+            <Panel header={t('question2')} key="2">
+              <div className="answers">{t('answer2')}</div>
+            </Panel>
+            <Panel header={t('question3')} key="3">
+              <div className="answers">{t('answer3')}</div>
+            </Panel>
+            <Panel header={t('question4')} key="4">
+              <div className="answers">{t('answer4')}</div>
+            </Panel>
+          </Collapse>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default faq;
