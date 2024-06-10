ff5d196807c7f8cfd857d7f4ed1145c82f7a9915
MeelanB <155341696+MeelanB@users.noreply.github.com>
2024-06-10 14:39:25 +0530

    Timeline table fixed columns added

--- web/src/Components/Timeline/timeline.tsx
+++ web/src/Components/Timeline/timeline.tsx
@@ -1,7 +1,8 @@
-import { Table, TableProps, Input } from 'antd';
+import { Table, TableProps, Input, Grid } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { ActualTimeline, ExpectedTimeline } from '../../Definitions/mtgTimeline.definition';
 import './timeline.scss';
+import { useEffect, useState } from 'react';
 
 interface Props {
   expectedTimeline: any;
@@ -16,6 +17,8 @@ interface Props {
   ) => void;
 }
 
+const { useBreakpoint } = Grid;
+
 const TimelineTable: React.FC<Props> = ({
   expectedTimeline,
   actualTimeline,
@@ -24,7 +27,20 @@ const TimelineTable: React.FC<Props> = ({
   onValueEnter,
 }) => {
   const { t } = useTranslation(['timelineTable']);
+  const screens = useBreakpoint();
+
   const isView: boolean = method === 'view' ? true : false;
+  const [allowFixedLegend, setAllowFixedLegend] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (screens.xxl) {
+      setAllowFixedLegend(true);
+    } else if (screens.xl) {
+      setAllowFixedLegend(true);
+    } else {
+      setAllowFixedLegend(false);
+    }
+  }, [screens]);
 
   const expectedTableColumns: TableProps<ExpectedTimeline>['columns'] = [
     {
@@ -33,12 +49,14 @@ const TimelineTable: React.FC<Props> = ({
       align: 'center',
       ellipsis: true,
       width: 100,
+      fixed: allowFixedLegend ? 'left' : undefined,
     },
     {
       title: t('catExpectedEmissionReduct'),
       dataIndex: 'topic',
       align: 'left',
       width: 350,
+      fixed: allowFixedLegend ? 'left' : undefined,
     },
     {
       title: t('total'),
@@ -48,6 +66,7 @@ const TimelineTable: React.FC<Props> = ({
       render: (colValue: any) => {
         return colValue;
       },
+      fixed: allowFixedLegend ? 'left' : undefined,
     },
   ];
 
@@ -58,12 +77,14 @@ const TimelineTable: React.FC<Props> = ({
       align: 'center',
       ellipsis: true,
       width: 100,
+      fixed: allowFixedLegend ? 'left' : undefined,
     },
     {
       title: t('catActualEmissionReduct'),
       dataIndex: 'topic',
       align: 'left',
       width: 350,
+      fixed: allowFixedLegend ? 'left' : undefined,
     },
     {
       title: t('total'),
@@ -73,6 +94,7 @@ const TimelineTable: React.FC<Props> = ({
       render: (colValue: any) => {
         return colValue;
       },
+      fixed: allowFixedLegend ? 'left' : undefined,
     },
   ];
 
@@ -131,12 +153,16 @@ const TimelineTable: React.FC<Props> = ({
         columns={expectedTableColumns}
         loading={loading}
         pagination={false}
+        scroll={{ y: 350 }}
+        className="custom-scroll-table"
       />
       <Table
         dataSource={actualTimeline}
         columns={actualTableColumns}
         loading={loading}
         pagination={false}
+        scroll={{ y: 210 }}
+        className="custom-scroll-table"
       />
     </div>
   );
