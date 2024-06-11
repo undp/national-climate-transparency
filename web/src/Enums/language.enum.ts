776930d52b6a3b45684408bb83fff60b6ac95c24
MeelanB <155341696+MeelanB@users.noreply.github.com>
2024-06-11 14:45:24 +0530

    Dynamic creation of available languages from enums added

---
+++ web/src/Enums/language.enum.ts
@@ -0,0 +1,11 @@
+export enum LanguageShortName {
+  ENGLISH = 'en',
+  FRENCH = 'fr',
+  JAPANESE = 'jp',
+}
+
+export enum LanguageFullName {
+  ENGLISH = 'English',
+  FRENCH = 'Français',
+  JAPANESE = 'Japanese',
+}
