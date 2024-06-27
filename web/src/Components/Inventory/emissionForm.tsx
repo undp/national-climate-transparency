79d6e5a6d177d5dcb58bb4d61b9930a1e26159e7
MeelanB <155341696+MeelanB@users.noreply.github.com>
2024-06-27 15:36:46 +0530

    Dynamic Section Population Added to Emission Component

--- web/src/Components/Inventory/emissionForm.tsx
+++ web/src/Components/Inventory/emissionForm.tsx
@@ -5,8 +5,13 @@ import { RcFile } from 'antd/lib/upload';
 import { AcceptedMimeTypes } from '../../Definitions/fileTypes';
 import { FileCard } from '../FileCard/fileCard';
 import { getBase64, getCollapseIcon } from '../../Utils/utilServices';
-import { EmissionSectorTitles, EmissionUnits } from '../../Enums/emission.enum';
+import { EmissionUnits } from '../../Enums/emission.enum';
 import NumberChip from '../NumberChip/numberChip';
+import {
+  SectionDefinition,
+  SubSectionsDefinition,
+  emissionSections,
+} from '../../Definitions/emissionDefinitions';
 
 interface Props {
   index: number;
@@ -51,7 +56,7 @@ export const EmissionForm: React.FC<Props> = ({ index, t, uploadedFile, setUploa
 
   return (
     <div className="emission-form">
-      <Row key={index} gutter={30} className="first-row">
+      <Row key={index} gutter={30} className="first-row" align={'middle'}>
         <Col span={8} className="height-column">
           <DatePicker className="year-picker" picker="year" size="middle" />
         </Col>
@@ -72,14 +77,14 @@ export const EmissionForm: React.FC<Props> = ({ index, t, uploadedFile, setUploa
           )}
         </Col>
       </Row>
-      <Row gutter={25} className="unit-row">
+      <Row gutter={25} className="unit-row" align={'middle'}>
         {Object.values(EmissionUnits).map((unit) => (
           <Col span={3}>
             <div className="unit-div">{unit}</div>
           </Col>
         ))}
       </Row>
-      <Row gutter={25} className="total-row">
+      <Row gutter={25} className="total-row" align={'middle'}>
         <Col className="total-div" span={12}>
           {'Total National Emissions and Removals '}
         </Col>
@@ -92,14 +97,14 @@ export const EmissionForm: React.FC<Props> = ({ index, t, uploadedFile, setUploa
       <Row className="collapsing-row">
         <Col span={24}>
           <Collapse ghost={true} expandIcon={({ isActive }) => getCollapseIcon(isActive ?? false)}>
-            {Object.values(EmissionSectorTitles).map((sectorTitle, sectorIndex) => (
+            {emissionSections.map((section: SectionDefinition, sectionIndex: number) => (
               <Panel
                 header={
                   <Row gutter={25} className="sector-header-row">
                     <Col className="title-div" span={12}>
                       <div style={{ display: 'flex', gap: '10px' }}>
-                        <span>{sectorIndex + 1}</span>
-                        <span>{sectorTitle}</span>
+                        <span>{sectionIndex + 1}</span>
+                        <span>{`${t(section.id)}_title`}</span>
                       </div>
                     </Col>
                     {Object.values(EmissionUnits).map((unit) => (
@@ -109,15 +114,69 @@ export const EmissionForm: React.FC<Props> = ({ index, t, uploadedFile, setUploa
                     ))}
                   </Row>
                 }
-                key={sectorTitle}
+                key={`emission_section${section.id}`}
               >
-                <p>{'Content'}</p>
+                {section.mainSections &&
+                  section.mainSections.map((mainSection: string) => (
+                    <Row gutter={25} className="input-number-row" align={'middle'}>
+                      <Col span={12} className="title-div">
+                        <div style={{ display: 'flex', gap: '10px' }}>
+                          <span>{t(mainSection)}</span>
+                          <span>{t(`${mainSection}_title`)}</span>
+                        </div>
+                      </Col>
+                      {Object.values(EmissionUnits).map(() => (
+                        <Col span={3} className="number-column">
+                          <Input type="number" min={0} step={0.01} className="input-emission" />
+                        </Col>
+                      ))}
+                    </Row>
+                  ))}
+                {section.subSections &&
+                  section.subSections.map((subSection: SubSectionsDefinition) => (
+                    <>
+                      <Row gutter={25} className="sector-sub-header-row" align={'middle'}>
+                        <Col className="title-div" span={12}>
+                          <div style={{ display: 'flex', gap: '10px' }}>
+                            <span>{t(subSection.id)}</span>
+                            <span>{t(`${subSection.id}_title`)}</span>
+                          </div>
+                        </Col>
+                        {Object.values(EmissionUnits).map((unit) => (
+                          <Col span={3}>
+                            <NumberChip value={100} valueType={unit} />
+                          </Col>
+                        ))}
+                      </Row>
+                      {subSection.sections &&
+                        subSection.sections.map((secondarySection: string) => (
+                          <Row gutter={25} className="input-number-row" align={'middle'}>
+                            <Col className="title-div" span={12}>
+                              <div style={{ display: 'flex', gap: '10px', paddingLeft: '50px' }}>
+                                <span>{t(secondarySection)}</span>
+                                <span>{t(`${secondarySection}_title`)}</span>
+                              </div>
+                            </Col>
+                            {Object.values(EmissionUnits).map(() => (
+                              <Col span={3} className="number-column">
+                                <Input
+                                  type="number"
+                                  min={0}
+                                  step={0.01}
+                                  className="input-emission"
+                                />
+                              </Col>
+                            ))}
+                          </Row>
+                        ))}
+                    </>
+                  ))}
               </Panel>
             ))}
           </Collapse>
         </Col>
       </Row>
-      <Row gutter={25} className="input-number-row">
+      <Row gutter={25} className="input-number-row" align={'middle'}>
         <Col className="title-div" span={12}>
           {'Total CO2 equivalent emissions without land use, land-use change and forestry'}
         </Col>
@@ -127,7 +186,7 @@ export const EmissionForm: React.FC<Props> = ({ index, t, uploadedFile, setUploa
           </Col>
         ))}
       </Row>
-      <Row gutter={25} className="input-number-row">
+      <Row gutter={25} className="input-number-row" align={'middle'}>
         <Col className="title-div" span={12}>
           {'Total CO2 equivalent emissions with land use, land-use change and forestry'}
         </Col>
---
+++ web/src/Definitions/emissionDefinitions.tsx
@@ -0,0 +1,30 @@
+export type SubSectionsDefinition = {
+  id: string;
+  sections: string[];
+};
+
+export type SectionDefinition = {
+  id: string;
+  hasSubSections: boolean;
+  mainSections: string[] | null;
+  subSections: SubSectionsDefinition[] | null;
+};
+
+export const emissionSections: SectionDefinition[] = [
+  {
+    id: '1',
+    hasSubSections: true,
+    mainSections: null,
+    subSections: [
+      { id: '1A', sections: ['1A1', '1A2', '1A3', '1A4', '1A5'] },
+      { id: '1B', sections: ['1B1', '1B2', '1B3'] },
+      { id: '1C', sections: ['1C1', '1C2', '1C3'] },
+    ],
+  },
+  {
+    id: '2',
+    hasSubSections: false,
+    mainSections: ['2A', '2B', '2C', '2D', '2E', '2F', '2F'],
+    subSections: null,
+  },
+];
