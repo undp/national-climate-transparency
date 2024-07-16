1e6d3c8698c9d5d95c869cd9279e493953a1265d
MeelanB <155341696+MeelanB@users.noreply.github.com>
2024-07-16 13:32:16 +0530

    FAQ Page Doc upload initial structures added

--- web/src/Pages/Faq/faq.tsx
+++ web/src/Pages/Faq/faq.tsx
@@ -1,26 +1,40 @@
 import { useTranslation } from 'react-i18next';
 import './faq.scss';
-import { Col, Collapse, Row } from 'antd';
-import FAQDocuments from '../../Components/FAQ/faqDocument';
-import { faqDocumentBps, faqVideoBps } from '../../Definitions/breakpoints/breakpoints';
+import { Button, Col, Collapse, Row } from 'antd';
+import { faqButtonBps, faqHeaderBps, faqVideoBps } from '../../Definitions/breakpoints/breakpoints';
+import { SystemResourceCategory, SystemResourceType } from '../../Enums/systemResource.enum';
+import { useState } from 'react';
+import { StoredData, UploadData } from '../../Definitions/uploadDefinitions';
+import UploadFileGrid from '../../Components/Upload/uploadFiles';
 
 const { Panel } = Collapse;
 
 const faq = () => {
-  const { t } = useTranslation(['faq']);
+  const { t } = useTranslation(['faq', 'entityAction']);
 
-  const documents = [
-    { key: '1', title: 'Document 1', format: 'docx', url: '/Assets/FAQ/Document1.docx' },
-    { key: '2', title: 'Document 2', format: 'xlsx', url: '/Assets/FAQ/Document2.xlsx' },
-    { key: '3', title: 'Document 3', format: 'txt', url: '/Assets/FAQ/Document3.txt' },
-    { key: '4', title: 'Document 4', format: 'pdf', url: '/Assets/FAQ/Document4.pdf' },
-  ];
+  const [isGhgEditEnabled, setIsGhgEditEnabled] = useState<boolean>(false);
+  const [isTemplateEditEnabled, setIsTemplateEditEnabled] = useState<boolean>(false);
+
+  const [uploadedGHGFiles, setUploadedGHGFiles] = useState<UploadData[]>([]);
+  const [storedGHGFiles, setStoredGHGFiles] = useState<StoredData[]>([]);
+  const [ghgFilesToRemove, setGhgFilesToRemove] = useState<string[]>([]);
+
+  const [uploadedTemplateFiles, setUploadedTemplateFiles] = useState<UploadData[]>([]);
+  const [storedTemplateFiles, setStoredTemplateFiles] = useState<StoredData[]>([]);
+  const [templateFilesToRemove, setTemplateFilesToRemove] = useState<string[]>([]);
 
   const videos = [
     { key: '1', title: 'Video 1', url: 'https://www.youtube.com/embed/_Y9vM4e9XaM' },
     { key: '2', title: 'Video 2', url: 'https://www.youtube.com/embed/cf1uzuXOJJw' },
     { key: '3', title: 'Video 3', url: 'https://www.youtube.com/embed/XfGSs4lN8Es' },
   ];
+
+  const getResource = (
+    resourceCategory: SystemResourceCategory,
+    resourceType: SystemResourceType
+  ) => {
+    console.log(resourceType, resourceCategory);
+  };
 
   return (
     <div className="content-container faq-container">
@@ -47,14 +61,72 @@ const faq = () => {
         </div>
       </div>
       <div className="content-card">
-        <div className="section-header">{t('docsAndTemplates')}</div>
-        <Row gutter={30} className="documents">
-          {documents.map((doc) => (
-            <Col key={doc.key} {...faqDocumentBps} className="grid-column">
-              <FAQDocuments title={doc.title} format={doc.format} url={doc.url} />
-            </Col>
-          ))}
+        <Row>
+          <Col {...faqHeaderBps} style={{ paddingTop: '6px' }}>
+            <div className="section-header">{t('ghgResourcesSection')}</div>
+          </Col>
+          <Col {...faqButtonBps}>
+            <Button
+              type="primary"
+              size="large"
+              block
+              style={{ padding: 0 }}
+              onClick={() => {
+                setIsGhgEditEnabled(!isGhgEditEnabled);
+                if (isGhgEditEnabled) {
+                  getResource(SystemResourceCategory.GHG, SystemResourceType.DOCUMENT);
+                }
+              }}
+            >
+              {isGhgEditEnabled ? t('entityAction:update') : t('entityAction:edit')}
+            </Button>
+          </Col>
         </Row>
+        <UploadFileGrid
+          isSingleColumn={false}
+          usedIn={isGhgEditEnabled ? 'update' : 'view'}
+          buttonText={t('entityAction:upload')}
+          storedFiles={storedGHGFiles}
+          uploadedFiles={uploadedGHGFiles}
+          setUploadedFiles={setUploadedGHGFiles}
+          removedFiles={ghgFilesToRemove}
+          setRemovedFiles={setGhgFilesToRemove}
+          setIsSaveButtonDisabled={console.log}
+        ></UploadFileGrid>
+      </div>
+      <div className="content-card">
+        <Row>
+          <Col {...faqHeaderBps} style={{ paddingTop: '6px' }}>
+            <div className="section-header">{t('docsAndTemplates')}</div>
+          </Col>
+          <Col {...faqButtonBps}>
+            <Button
+              type="primary"
+              size="large"
+              block
+              style={{ padding: 0 }}
+              onClick={() => {
+                setIsTemplateEditEnabled(!isTemplateEditEnabled);
+                if (isTemplateEditEnabled) {
+                  getResource(SystemResourceCategory.FAQ, SystemResourceType.DOCUMENT);
+                }
+              }}
+            >
+              {isTemplateEditEnabled ? t('entityAction:update') : t('entityAction:edit')}
+            </Button>
+          </Col>
+        </Row>
+        <UploadFileGrid
+          isSingleColumn={false}
+          usedIn={isTemplateEditEnabled ? 'update' : 'view'}
+          buttonText={t('entityAction:upload')}
+          storedFiles={storedTemplateFiles}
+          uploadedFiles={uploadedTemplateFiles}
+          setUploadedFiles={setUploadedTemplateFiles}
+          removedFiles={templateFilesToRemove}
+          setRemovedFiles={setTemplateFilesToRemove}
+          setIsSaveButtonDisabled={console.log}
+        ></UploadFileGrid>
       </div>
       <div className="content-card">
         <div className="section-header">{t('trainingVideos')}</div>
