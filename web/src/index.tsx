da3a1eed3d74519c2473b8fbb5d2c3f3b5dd077f
MeelanB <155341696+MeelanB@users.noreply.github.com>
2024-07-24 10:06:53 +0530

    Added a Suspense wrapper for the App Rendering

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './Components/Internationalization/i18n';
+import { Loading } from './Components/Loading/loading';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<Loading />}>
+      <App />
+    </Suspense>
   </React.StrictMode>
 );
